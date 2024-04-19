76e260aa3b02afcbb941202cdc56b22fd22abb0b
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2024-04-19 14:59:59 +0100

    Updates cluster_genomes.py with subsampling

--- cluster_genomes.py
+++ cluster_genomes.py
@@ -1,14 +1,13 @@
 from scipy.spatial.distance import pdist
 from scipy.spatial import distance
 import numpy as np
-from sklearn.cluster import MeanShift
 from sklearn.neighbors import KernelDensity
 from sklearn.model_selection import GridSearchCV
 from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 import networkx as nx
+from scipy.cluster.hierarchy import single, fcluster, dendrogram
 
-from sklearn.base import BaseEstimator, ClassifierMixin
 
 
 def main():
@@ -17,11 +16,14 @@ def main():
     #infile = options.infile
     #plot = options.plot
     #outpref = options.outpref
+    #cutoff = options.cutoff
 
     #for debugging
-    infile = "tokenised_genomes_test.txt"
+    infile = "tokenised_genomes.txt"
     plot = True
     outpref = "synteny"
+    subsample = 100000
+    kde_bandwidth = 0.03
 
     genome_list = []
     genome_name_list = []
@@ -55,16 +57,38 @@ def main():
         genome_array[list(genome)] = 1
         pangenome_array[genome_idx] = genome_array
 
+    print("Calculating Jaccard distances...")
     jaccard_acc = pdist(pangenome_array, metric='jaccard')
     #print(len(jaccard_acc))
 
-    # optimise bandiwth for Kernel density
-    bandwidths = 10 ** np.linspace(-2, 1, 10)
-    grid = GridSearchCV(KernelDensity(kernel='gaussian'),{'bandwidth': bandwidths})
-    grid.fit(jaccard_acc[:, None])
+    if plot == True:
+        # Plot the data and density function with identified peaks
+        plt.figure(figsize=(12, 6))
+        plt.hist(jaccard_acc, bins=50, density=True, alpha=0.5, label='Histogram of Data')
 
-    kde = grid.best_estimator_
+        plt.title('Data Distribution')
+        plt.xlabel('Accessory Distance')
+        plt.ylabel('Density')
+        plt.legend()
+        plt.show()
+
+        plt.savefig(outpref + "_hist.png")
+
+    # optimise bandiwth for Kernel density
+    # subsample to N
+    jaccard_acc_sample = np.random.choice(jaccard_acc, subsample)
+
+    
+    print("Fitting KDE model...")
+    # bandwidth grid search
+    #bandwidths = 10 ** np.linspace(-2, 1, 10)
+    #grid = GridSearchCV(KernelDensity(kernel='gaussian'),{'bandwidth': bandwidths})
+    #grid.fit(jaccard_acc_sample[:, None])
+    #kde = grid.best_estimator_
+    
     #print(kde)
+    kde = KernelDensity(bandwidth=kde_bandwidth, kernel='epanechnikov')
+    kde.fit(jaccard_acc_sample[:, None])
     x = np.linspace(0, 1000, 1000)[:, None] / 1000
     log_density = kde.score_samples(x)
     density = np.exp(log_density)
@@ -74,14 +98,33 @@ def main():
     cluster_centers = x[maxima_indices]
     cluster_centers = cluster_centers.T[0]
     cluster_centers = np.sort(cluster_centers)
+
+    # get first and second cluster
+    print("Assigning clusters...")
+    clusters = np.zeros(len(jaccard_acc), dtype=int)
+    print("Cluster centers: {}".format(cluster_centers))
+    if len(cluster_centers) > 1:
+        cutoff = (cluster_centers[0] + cluster_centers[1]) / 2
+        print("Accessory Jaccard cutoff: {}".format(cutoff))
+        clusters = np.zeros(len(jaccard_acc), dtype=int)
+        # assign as cluster 1 if above cutoff
+        mask = jaccard_acc > cutoff
+        clusters[mask] = 1
+    else:
+        print("No clusters detected. All same strain.")
+
     #print(cluster_centers)
 
     # assign clusters
-    clusters = np.abs(jaccard_acc[:, np.newaxis] - cluster_centers).argmin(axis=1)
-    #print(len(clusters))
+    #clusters = np.abs(jaccard_acc[:, np.newaxis] - cluster_centers).argmin(axis=1)
 
+    # generate square plot to allow indexing
+    clusters = distance.squareform(clusters)
+    #print(clusters)
+    
     if plot == True:
         # Plot the data and density function with identified peaks
+        plt.close()
         plt.figure(figsize=(12, 6))
         plt.hist(jaccard_acc, bins=50, density=True, alpha=0.5, label='Histogram of Data')
         plt.plot(x, density, label='Kernel Density Estimation')
@@ -95,8 +138,6 @@ def main():
 
         plt.savefig(outpref + "_kernel_plot.png")
 
-    # generate square plot to allow indexing
-    clusters = distance.squareform(clusters)
     #print(clusters)
 
     # iterate over upper half with diagonal
@@ -104,6 +145,8 @@ def main():
     #print(rows)
     #print(columns)
     G = nx.Graph()
+
+    print("Generating network...")
 
     for row in range(rows):
         for col in range(row, columns):
@@ -116,6 +159,7 @@ def main():
 
     graph_clusters = list(nx.connected_components(G))
 
+    print("Writing output...")
     #print(graph_clusters)
     # write out clusters of genomes
     with open(outpref + "_clusters.txt", "w") as f:
