f9d65c0719cdf2e0f3587ab4b840fe05aa23335d
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2024-04-19 15:52:25 +0100

    Allows stratified sampling by genome clusters

--- prepare_synteny.py
+++ prepare_synteny.py
@@ -1,11 +1,12 @@
 import os
 import pickle
-import requests
-import numpy as np
 from tokenizers import Tokenizer, models, pre_tokenizers, trainers
 from sklearn.model_selection import train_test_split
 from random import shuffle
 from panGPT import GenomeDataset
+import random
+import math
+from torch.utils.data import Dataset
 
 # Define a custom pre-tokenization rule using a function
 def custom_pre_tokenize(text):
@@ -14,29 +15,63 @@ def custom_pre_tokenize(text):
 
 # download the tiny shakespeare dataset
 input_file_path = "tokenised_genomes.txt"
+cluster_file_path = "synteny_clusters.txt"
+
+cluster_dict = {}
+with open(cluster_file_path, 'r') as f:
+    while True:
+            line = f.readline()
+            if not line:
+                break
+            split_line = line.rstrip().split("\t")
+            cluster = split_line[0]
+            genome_name = split_line[1]
+            if cluster not in cluster_dict:
+                cluster_dict[cluster] = []
+            cluster_dict[cluster].append(genome_name)
+
+# generate test and training data
+training_split = {}
+for cluster, genomes in cluster_dict.items():
+    training =  set(random.sample(genomes, math.ceil(len(genomes) * 0.8)))
+    val = set(genomes) - training
+
+    # training is 1, val is 0
+    for entry in training:
+         training_split[entry] = 1
+    for entry in val:
+         training_split[entry] = 0
 
 unique_chars = set()
 genomes = []
+train_genomes = []
+val_genomes = []
 with open(input_file_path, 'r') as f:
-    for line in f:
-        while True:
-                line = f.readline()
-                if not line:
-                    break
-                split_line = " " + line.rstrip().split("\t")[1] + " "
+    while True:
+            line = f.readline()
+            if not line:
+                break
+            split_line = line.rstrip().split("\t")
+            genome_name = split_line[0]
+            split_line = " " + split_line[1] + " "
 
-                contigs = split_line.split("_")
-                # remove empty contigs
-                contigs = [x for x in contigs if x != " "]
+            contigs = split_line.split("_")
+            # remove empty contigs
+            contigs = [x for x in contigs if x != " "]
 
-                # shuffle contig order
-                shuffle(contigs)
+            # shuffle contig order
+            shuffle(contigs)
 
-                contigs = "_".join(contigs)
+            contigs = "_".join(contigs)
 
-                # add end of contig and assembly tokens
-                sequence = "[START] _" + contigs + "_ [END]"
-                genomes.append(sequence)
+            # add end of contig and assembly tokens
+            sequence = "[START] _" + contigs + "_ [END]"
+            genomes.append(sequence)
+
+            if training_split[genome_name] == 1:
+                train_genomes.append(sequence)
+            else:
+                val_genomes.append(sequence)
 
 # Code from PanGPT (https://github.com/mol-evol/panGPT) developed by James McInerney
 # Initialize and train the tokenizer using the 'tokenizers' library
@@ -52,7 +87,7 @@ tokenizer.train_from_iterator(genomes, trainer)
 tokenizer.save("tokens.bin")  # Save the trained tokenizer
 
 # Split the data into training and validation sets (80% training, 20% validation)
-train_genomes, val_genomes = train_test_split(genomes, test_size=0.2, random_state=42)
+#train_genomes, val_genomes = train_test_split(genomes, test_size=0.2, random_state=42)
 
 train_dataset = GenomeDataset(train_genomes, tokenizer)
 val_dataset = GenomeDataset(val_genomes, tokenizer)
