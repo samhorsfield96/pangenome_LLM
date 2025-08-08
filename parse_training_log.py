75250d308d279ba56d4e7c3c03870acd07f92410
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-08-08 16:01:17 +0100

    Corrects parsing of training logs if line writing corrupted

--- parse_training_log.py
+++ parse_training_log.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 def get_options():
     description = "Compares synteny between simulated and generated genomes"
@@ -30,49 +31,62 @@ def main():
                 continue
 
             # format line
-            split_line = line.rstrip().replace(',', '').split()
-            if "Test" in line:
-                loss = split_line[2]
-                perplexity = split_line[4]
-                accuracy = split_line[6]
-                precision = split_line[8]
-                recall = split_line[10]
-                F1 = split_line[12]
-                kappa = split_line[14]
-                tmp_tuple = (loss, perplexity, accuracy, precision, recall, F1, kappa)
-                test_list.append(tmp_tuple)
+            split_line = line.rstrip().split()
+            if "Test Loss" in line:
+                match = re.search(r'Test Loss:\s*(.*)', line)
+                if match:
+                    result = match.group(1)
+                    split_result = result.rstrip().replace(',', '').split()
+                    loss = split_result[0]
+                    perplexity = split_result[2]
+                    accuracy = split_result[4]
+                    precision = split_result[6]
+                    recall = split_result[8]
+                    F1 = split_result[10]
+                    tmp_tuple = (loss, perplexity, accuracy, precision, recall, F1)
+                    test_list.append(tmp_tuple)
             else: 
                 curr_epoch = split_line[1]
                 # check if at new epoch
                 if prev_epoch != curr_epoch:
                     epoch += 1
                     prev_epoch = curr_epoch
-                loss = split_line[5]
-                perplexity = split_line[7]
-                if "Training" in line:
-                    learning_rate = split_line[10]
-                    tmp_tuple = (epoch, loss, perplexity, learning_rate)
-                    training_list.append(tmp_tuple)
-                elif "Validation" in line:
-                    accuracy = split_line[9]
-                    precision = split_line[11]
-                    recall = split_line[13]
-                    F1 = split_line[15]
-                    kappa = split_line[17]
-                    tmp_tuple = (epoch, loss, perplexity, accuracy, precision, recall, F1, kappa)
-                    validation_list.append(tmp_tuple)
+                if "Training Loss" in line:
+                    match = re.search(r'Training Loss:\s*(.*)', line)
+                    if match:
+                        result = match.group(1)
+                        split_result = result.rstrip().replace(',', '').split()
+                        #print(result)
+                        loss = split_result[0]
+                        perplexity = split_result[2]
+                        learning_rate = split_result[5]
+                        tmp_tuple = (epoch, loss, perplexity, learning_rate)
+                        training_list.append(tmp_tuple)
+                elif "Validation Loss" in line:
+                    match = re.search(r'Validation Loss:\s*(.*)', line)
+                    if match:
+                        result = match.group(1)
+                        split_result = result.rstrip().replace(',', '').split()
+                        loss = split_result[0]
+                        perplexity = split_result[2]
+                        accuracy = split_result[4]
+                        precision = split_result[6]
+                        recall = split_result[8]
+                        F1 = split_result[10]
+                        tmp_tuple = (epoch, loss, perplexity, accuracy, precision, recall, F1)
+                        validation_list.append(tmp_tuple)
 
     with open(outfile, "w") as o:
-        o.write("Type\tEpoch\tLoss\tPerplexity\tLearning_rate\tAccuracy\tPrecision\tRecall\tF1\tKappa\n")
+        o.write("Type\tEpoch\tLoss\tPerplexity\tLearning_rate\tAccuracy\tPrecision\tRecall\tF1\n")
         for index, entry in enumerate(validation_list):
-            epoch, loss, perplexity, accuracy, precision, recall, F1, kappa = entry
-            o.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("Validation", epoch, loss, perplexity, training_list[index][3], accuracy, precision, recall, F1, kappa))
+            epoch, loss, perplexity, accuracy, precision, recall, F1 = entry
+            o.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("Validation", epoch, loss, perplexity, training_list[index][3], accuracy, precision, recall, F1))
         for entry in training_list:
             epoch, loss, perplexity, learning_rate = entry
-            o.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("Training", epoch, loss, perplexity, learning_rate, "NA", "NA", "NA", "NA", "NA"))
+            o.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("Training", epoch, loss, perplexity, learning_rate, "NA", "NA", "NA", "NA"))
         for entry in test_list:
-            loss, perplexity, accuracy, precision, recall, F1, kappa = entry
-            o.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("Test", "NA", loss, perplexity, "NA", accuracy, precision, recall, F1, kappa))
+            loss, perplexity, accuracy, precision, recall, F1 = entry
+            o.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("Test", "NA", loss, perplexity, "NA", accuracy, precision, recall, F1))
 
 if __name__ == "__main__":
     main()
